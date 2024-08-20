6d8ae3bc60054898b23e59ce334b617ff12e51b6
Rawgher <rdpender3@gmail.com>
2024-08-20 14:18:34 -0700

    company routes working

--- db.js
+++ db.js
@@ -1,2 +1,19 @@
 /** Database setup for BizTime. */
+const { Client } = require("pg");
+require('dotenv').config()
 
+// const DB_URI = (process.env.NODE_ENV === "test")
+//   ? "postgresql:///biztime_test"
+//   : "postgresql:///biztime";
+
+const DB_URI = (process.env.NODE_ENV === "test")
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL;
+
+let db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
---
+++ routes/companies.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = new express.Router();
+const db = require('../db');
+const ExpressError = require('../expressError');
+
+// Get /
+router.get('/', async (req, res, next) => {
+    try {
+        const results = await db.query(`SELECT code, name FROM companies`);
+        return res.json({ companies: results.rows });
+    } catch (e) {
+        next(e);
+    }
+})
+
+
+// Get /:code
+router.get('/:code', async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [code]);
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't find company with code of ${code}`, 404); 
+        }
+        return res.json({ company: results.rows[0] });
+    } catch (e) {
+        next(e);
+    }
+})
+
+
+// Post /
+router.post('/', async (req, res, next) => {
+    try {
+        const { code, name, description } = req.body;
+        const results = await db.query(`INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING *`, [code, name, description]);
+        return res.status(201).json({ company: results.rows[0] })
+        
+    } catch (e) {
+        next(e);
+    }
+})
+
+
+// Put /:code
+router.put('/:code', async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const { name, description } = req.body;
+        const results = await db.query(`UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING *`, [name, description, code]);
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't update company with code of ${code}`, 404); 
+        }
+        return res.json({ company: results.rows[0] });
+    } catch (e) {
+        next(e);
+    }
+})
+
+
+// Delete /:code
+router.delete('/:code', async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const results = db.query(`DELETE FROM companies WHERE code=$1`, [code]);
+        return res.json({ msg: 'Company Deleted!'})
+    } catch (e) {
+        next(e);
+    }
+})
+
+module.exports = router;
